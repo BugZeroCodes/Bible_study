0aaca57383d679254694999a33de82931fb99be9
Timothy Amadi <timothy@Timothys-MacBook-Air.local>
2017-08-26 10:36:28 -0400

    Amend

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
   get 'search' => 'bible_verses#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814001500) do
+ActiveRecord::Schema.define(version: 20170820215813) do
 
   create_table "answers", force: :cascade do |t|
     t.text "text"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170814001500) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.string "username"
+    t.boolean "theme_level"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
