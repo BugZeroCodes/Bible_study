6f7d46b6dccf6e68b3233a40d0a555360154cc40
Timothy Amadi <timothy@Timothys-MacBook-Air.local>
2017-08-26 11:41:26 -0400

    gh

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :bible_verses
   post 'bible_verses/:id/grade' => 'bible_verses#grade', as: :grade
 
-  root 'bible_verses#index'
+  root 'questions#index'
 
   get 'search' => 'bible_verses#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
