83923304266138c2b0e5e40ba6320abab12d98a0
Timothy Amadi <timothy@Timothys-Air.attlocal.net>
2017-09-23 11:50:35 -0400

    fghfdfh

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,10 @@
 class QuestionsController < ApplicationController
   def index
-\\
   end
-
   def by_level
-
+    levels = ['developers', 'detectives', 'defenders']
+    if !levels.include? params[:level]
+      redirect_to :root
+    end
   end
 end
