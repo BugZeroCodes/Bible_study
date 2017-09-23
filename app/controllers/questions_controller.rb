e7a6a87bdc1dbbb73cd4b8c01a40d97f6900ab45
Timothy Amadi <timothy@Timothys-Air.attlocal.net>
2017-09-23 10:49:52 -0400

    erg

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,9 @@
 class QuestionsController < ApplicationController
   def index
-    @themes = Question.group(:theme)
-    @questions = Question.group(:level)
+\\
   end
 
   def by_level
-    
+
   end
 end
